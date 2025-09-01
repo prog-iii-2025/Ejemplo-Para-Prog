780470b4c442f11a5dde80c1de3db148b190f818
paulagfb <paulagferreirab@gmail.com>
2025-09-01 13:44:11 -0300

    Update main.java
    saqué los comentarios

--- src/main.java
+++ src/main.java
@@ -1,15 +1,8 @@
 
-/**
- *
- * @author Carlos Álvarez
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.println("Hola Prog III!!!");
     }
     
