8705c263324c19fd202c8a9e0d8d5fbac0437c8a
Paula <paulagferreirab@gmail.com>
2025-09-01 14:04:28 -0300

    ss

---
+++ src/OtraClase.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class OtraClase {
+    
+    public void metodo() {
+        System.out.println("paula creó esta clase");
+    }
+    
+}
